acfe0f4f35fdf36da0dd1ecbdba36e0404a9df48
YRabbit <rabbit@yrabbit.cyou>
2025-03-30 11:59:02 +1000

    MUX_4, MUX8

--- techlibs/gowin/cells_xtra_gw2a.v
+++ techlibs/gowin/cells_xtra_gw2a.v
@@ -1,6 +1,20 @@
 // Created by cells_xtra.py
 
 
+module MUX4 (...);
+input I0, I1, I2, I3;
+input S0, S1;
+output O;
+endmodule
+
+
+module MUX8 (...);
+input I0, I1, I2, I3, I4, I5, I6, I7;
+input S0, S1, S2;
+output O;
+endmodule
+
+
 module LUT5 (...);
 parameter INIT = 32'h00000000;
 input I0, I1, I2, I3, I4;
